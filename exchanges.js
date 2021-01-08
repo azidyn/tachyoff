879701eba7576676fdfb9c8c6c816c1bebfaeb4d
Alec Azimuth <azidyn@protonmail.com>
2021-01-08 19:12:11 +0000

    add binance spot

--- exchanges.js
+++ exchanges.js
@@ -7,6 +7,7 @@ module.exports = {
 
     bitmex:                 'https://www.bitmex.com/api/v1/orderBook/L2?symbol=XBTUSD&depth=5',
     'binance-futures':      'https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit=5',
+    'binance-spot':         'https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=5',
     ftx:                    'https://ftx.com/api/markets/BTC-PERP/orderbook?depth=5',
     deribit:                'https://www.deribit.com/api/v2/public/get_order_book?depth=5&instrument_name=BTC-PERPETUAL',
     okex:                   'https://www.okex.com/api/swap/v3/instruments/BTC-USDT-SWAP/depth?size=5',
