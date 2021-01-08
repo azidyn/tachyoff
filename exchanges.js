8f0806e2932f6d068beac014eeeafc18797322ef
Alec Azimuth <azidyn@protonmail.com>
2021-01-08 19:10:28 +0000

    add deribit, okex, bybit, coinbase and change request to the L2 orderbook

--- exchanges.js
+++ exchanges.js
@@ -1,11 +1,16 @@
 
 // A list of exchange GET endpoints to sample
-// They each attempt to grab the latest 1min bar from the exchange.
+
+// Test to snatch 5 levels of the orderbook, except where noted
 
 module.exports = {
 
-    bitmex:                 'https://www.bitmex.com/api/v1/trade/bucketed?binSize=1m&partial=true&symbol=XBTUSD&count=1&reverse=true',
-    'binance-futures':      'https://fapi.binance.com/fapi/v1/klines?symbol=BTCUSDT&interval=1m&limit=1',
-    ftx:                    'https://ftx.com/api/markets/BTC-PERP/candles?resolution=300&limit=1'
+    bitmex:                 'https://www.bitmex.com/api/v1/orderBook/L2?symbol=XBTUSD&depth=5',
+    'binance-futures':      'https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit=5',
+    ftx:                    'https://ftx.com/api/markets/BTC-PERP/orderbook?depth=5',
+    deribit:                'https://www.deribit.com/api/v2/public/get_order_book?depth=5&instrument_name=BTC-PERPETUAL',
+    okex:                   'https://www.okex.com/api/swap/v3/instruments/BTC-USDT-SWAP/depth?size=5',
+    'bybit-inverse':        'https://api.bybit.com/v2/public/orderBook/L2?symbol=BTCUSD&depth=5',
+    coinbase:               'https://api.pro.coinbase.com/products/BTC-USD/book?level=1',                                       // Just a quote, not 5 levels
 
 }
